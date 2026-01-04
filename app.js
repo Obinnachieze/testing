e192e136775a228a131610cff00d6c43356cfec4
Obinnachieze <wisdomchieze0@gmail.com>
2026-01-04 14:03:06 +0100

    done

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const meters = document.getElementById("num");
 const p1 = document.getElementById("p1");
 const p2 = document.getElementById("p2");
 const p3 = document.getElementById("p3");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  let dig = meters.value;
+  const met = dig / 3.28084;
+  p1.textContent = `${dig} = ${Number(met.toFixed(3))}m `;
+  const liters = dig * 3.785;
+  p2.textContent = `${dig} = ${Number(liters.toFixed(3))}lt `;
+  const kilo = dig * 0.453592;
+  p3.textContent = `${dig} = ${Number(kilo.toFixed(3))}kg `;
+  meters.value = "";
+});
